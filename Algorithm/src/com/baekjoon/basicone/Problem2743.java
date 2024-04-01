11805d4d654b7734e1ed3c603582608d6ed7826d
zingzing <owj20127@gmail.com>
2024-04-01 22:11:12 +0900

    백준 2743문제 - 단어 길이 재기 문제 풀이

---
+++ Algorithm/src/com/baekjoon/basicone/Problem2743.java
@@ -0,0 +1,13 @@
+package com.baekjoon.basicone;
+
+import java.io.*;
+
+public class Problem2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(input.length());
+    }
+}
